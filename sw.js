0cb8e7ed54cd5ee568e33e39b705102a4bfa34b6
Bernio <jendabernard@gmail.com>
2026-04-30 21:04:15 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    - Update: pro stejné origin soubory vracíme cache hned, ale na pozadí dotahujeme novou verzi a ukládáme do cache.
    - UI může vynutit okamžitou aktivaci přes message {type:'SKIP_WAITING'}.
 */
-const CACHE_NAME = "noise-pwa-v22";
+const CACHE_NAME = "noise-pwa-v23";
 const ASSETS = [
   "./",
   "./index.html",
